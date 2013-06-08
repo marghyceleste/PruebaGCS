ff15ae1e0edb9ee3bf65955e374aff0b7f34fd58
margothTorralba <princes03_08@hotmail.com>
2013-06-08 11:08:07 -0400

    Esta es la inicializacion de la prueba del proyecto usando git

---
+++ src/programaejemplo/prueba.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programaejemplo;
+
+/**
+ *
+ * @author Patricia
+ */
+public class prueba {
+int a=2;
+int b=4;
+public void Suma(){
+    System.out.println("hola que tal");
+}         
+}
